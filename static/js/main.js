cc37b47b41867e11acd10ae0c8aa4630df894241
Kavish Shah <kavishshah09@gmail.com>
2020-05-27 21:35:50 +0530

    confirmation on delete

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+console.log('hello');
+
+$(document).ready(function () {
+	$('.delete-transaction').click(function () {
+		var id = $(this).attr('data-id');
+		var url = $(this).attr('data-url');
+
+		$('.modal-form', 'input').val(id);
+		$('.modal-form').attr('action', url);
+	});
+});
