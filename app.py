5a7f0b93cde41087d84d5547dcddf1c5d80bfa49
Kavish Shah <kavishshah09@gmail.com>
2020-05-30 14:29:17 +0530

    fixed code

--- app.py
+++ app.py
@@ -8,11 +8,14 @@ import timeago
 import datetime
 from wtforms.fields.html5 import EmailField
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask_mail import Mail, Message
 
 app = Flask(__name__, static_url_path='/static')
 app.config.from_pyfile('config.py')
 
 mysql = MySQL(app)
+mail = Mail(app)
+
 
 @app.route('/')
 def index():
@@ -27,7 +30,8 @@ def about():
 class SignUpForm(Form):
     first_name = StringField('First Name', [validators.Length(min=1, max=100)])
     last_name = StringField('Last Name', [validators.Length(min=1, max=100)])
-    email = EmailField('Email address', [validators.DataRequired(), validators.Email()])
+    email = EmailField('Email address', [
+                       validators.DataRequired(), validators.Email()])
     username = StringField('Username', [validators.Length(min=4, max=100)])
     password = PasswordField('Password', [
         validators.DataRequired(),
@@ -50,7 +54,7 @@ def signup():
         password = sha256_crypt.encrypt(str(form.password.data))
 
         cur = mysql.connection.cursor()
-        result = cur.execute("SELECT * FROM users WHERE email=%s",[email])
+        result = cur.execute("SELECT * FROM users WHERE email=%s", [email])
         if result > 0:
             flash('The entered email address has already been taken.Please try using or creating another one.', 'info')
             return redirect(url_for('signup'))
@@ -396,8 +400,11 @@ def deleteCurrentMonthTransaction(id):
 
     return redirect(url_for('addTransactions'))
 
+
 class RequestResetForm(Form):
-    email = EmailField('Email address', [validators.DataRequired(), validators.Email()])
+    email = EmailField('Email address', [
+                       validators.DataRequired(), validators.Email()])
+
 
 @app.route("/reset_request", methods=['GET', 'POST'])
 def reset_request():
@@ -408,59 +415,32 @@ def reset_request():
     if request.method == 'POST' and form.validate():
         email = form.email.data
         cur = mysql.connection.cursor()
-        result = cur.execute("SELECT * FROM users WHERE email = %s", [email])
-        cur.execute("SELECT username,email FROM users WHERE email = %s", [email])
-        data = cur.fetchone()
-        user = data['username']
-        email = data['email']
-        cur.close()
+        result = cur.execute(
+            "SELECT id,username,email FROM users WHERE email = %s", [email])
         if result == 0:
-            flash('There is no account with that email. You must register first.', 'warning')
+            flash(
+                'There is no account with that email. You must register first.', 'warning')
             return redirect(url_for('signup'))
         else:
-            send_reset_email(user,email)
-            flash('An email has been sent with instructions to reset your password.', 'info')
-            return redirect(url_for('login'))
-    return render_template('reset_request.html',form=form)
-
-"""
-def get_reset_token(self, expires_sec=1800):
-        cur = mysql.connection.cursor()
-        result = cur.execute("SELECT id FROM users")
-        if result>0:
-            id = cur.fetchall()
-        s = Serializer(app.config['SECRET_KEY'], expires_sec)
-        return s.dumps({'user_id': self.id}).decode('utf-8')
-
-@staticmethod
-def verify_reset_token(token):
-    s = Serializer(app.config['SECRET_KEY'])
-    try:
-        user_id = s.loads(token)['user_id']
-    except:
-        return None
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM users WHERE id = %s", [user_id])
-    data = cur.fetchone()
-    cur.close()
-    return data
-"""
-    
-def send_reset_email(user,email):
-    print(user)
-    print(email)
-"""
-    token = user.get_reset_token()
-    msg = Message('Password Reset Request',
-                  sender='noreply@demo.com',
-                  recipients=[user.email])
-    msg.body = f'''To reset your password, visit the following link:
+            data = cur.fetchone()
+            user_id = data['id']
+            user_email = data['email']
+            cur.close()
+            s = Serializer(app.config['SECRET_KEY'], 1800)
+            token = s.dumps({'user_id': user_id}).decode('utf-8')
+            msg = Message('Password Reset Request',
+                          sender='noreply@demo.com', recipients=[user_email])
+            msg.body = f'''To reset your password, visit the following link:
 {url_for('reset_token', token=token, _external=True)}
 If you did not make password reset request then simply ignore this email and no changes will be made.
 Note:This link is valid only for 30 mins from the time you requested a password change request.
 '''
-    mail.send(msg)
-"""
+            mail.send(msg)
+            flash(
+                'An email has been sent with instructions to reset your password.', 'info')
+            return redirect(url_for('login'))
+    return render_template('reset_request.html', form=form)
+
 
 class ResetPasswordForm(Form):
     password = PasswordField('Password', [
@@ -469,27 +449,35 @@ class ResetPasswordForm(Form):
     ])
     confirm = PasswordField('Confirm Password')
 
-"""
+
 @app.route("/reset_password/<token>", methods=['GET', 'POST'])
 def reset_token(token):
     if 'logged_in' in session and session['logged_in'] == True:
         flash('You are already logged in', 'info')
         return redirect(url_for('index'))
-    user = data.verify_reset_token(token)
-    if user is None:
+    s = Serializer(app.config['SECRET_KEY'])
+    try:
+        user_id = s.loads(token)['user_id']
+    except:
         flash('That is an invalid or expired token', 'warning')
         return redirect(url_for('reset_request'))
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT id FROM users WHERE id = %s", [user_id])
+    data = cur.fetchone()
+    cur.close()
+    user_id = data['id']
     form = ResetPasswordForm(request.form)
     if request.method == 'POST' and form.validate():
         password = sha256_crypt.encrypt(str(form.password.data))
         cur = mysql.connection.cursor()
-        #query for updating the password for that particular user
+        cur.execute(
+            "UPDATE users SET password = %s WHERE id = %s", (password, user_id))
         mysql.connection.commit()
         cur.close()
         flash('Your password has been updated! You are now able to log in', 'success')
         return redirect(url_for('login'))
     return render_template('reset_token.html', title='Reset Password', form=form)
-"""
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 import os
 
 DEBUG = True
-TESTING = True
+# TESTING = True
 MYSQL_HOST = 'localhost'
 MYSQL_USER = 'root'
 MYSQL_PASSWORD = os.getenv('MYSQL_PWD')
 MYSQL_DB = 'tracker'
 MYSQL_CURSORCLASS = 'DictCursor'
 SECRET_KEY = 'your_secret_key'
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = os.getenv('EMAIL_USER')
+MAIL_PASSWORD = os.getenv('EMAIL_PASS')
